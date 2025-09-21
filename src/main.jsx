0d29c039b86262912d7b28c3bf6f77b8433489c0
Israel2129 <nannissac@gmail.com>
2025-09-21 01:39:13 -0600

    carrito

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { CarritoApp } from './CarritoApp'
+import { BrowserRouter } from 'react-router-dom'
+import './styles/card.css'
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <StrictMode>
+            <CarritoApp />
+        </StrictMode>,
+    </BrowserRouter>
+)
